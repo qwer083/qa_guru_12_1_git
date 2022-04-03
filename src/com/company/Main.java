ac336e13f1824ed4ffb0b22524c18114a780d698
Anna <89787891+qwer083@users.noreply.github.com>
2022-04-03 20:48:41 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,4 +6,8 @@ public class Main {
         System.out.println("Hello, QA.guru!");
         System.out.println("Another line1");
     }
+    
+    public static void another(){
+        System.out.println("Aloha");
+    }
 }
